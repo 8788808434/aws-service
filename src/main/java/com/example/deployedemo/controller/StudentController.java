7690f362faac9244435a86444fcb70a5ebbe4d50
8788808434 <AP4447628@GMAIL.COM>
2022-08-05 03:59:06 +0530

    save 12

--- src/main/java/com/example/deployedemo/controller/StudentController.java
+++ src/main/java/com/example/deployedemo/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.deployedemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+   @GetMapping("/get")
     public String get()
     {
         return "hello aws";
